d09432248cb0a4eb92783821986cc30522fdf43e
solgamd <solgamd@gmail.com>
2020-02-05 17:39:16 -0600

    Removed button and toggle

--- src/components/button/button.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './button.css';
-
-function Button({label}){
-    return <div data-testid="button" className="btn-style btn">Hi {label}</div>
-}
-
-export default Button;
