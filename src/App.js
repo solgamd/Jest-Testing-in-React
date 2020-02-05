f52d853c4103349c693cf0ace631e893f77d7002
solgamd <solgamd@gmail.com>
2020-02-05 17:44:41 -0600

    Built App with counter functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0
+    }
+  };
+  makeIncrementer = (amount) => {
+    this.setState(prevState => ({
+      count: prevState.count + amount
+    }));
+  };
+
+  increment = this.makeIncrementer(1);
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Count: {this.state.count}</h1>
+        <button className="increment" onClick={this.increment}>Increment Count</button>
+      </div>
+    );
+  }
 }
 
 export default App;
