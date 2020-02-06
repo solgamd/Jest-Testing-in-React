bf46b9cfc1fecc916c977762c907649889e76899
solgamd <solgamd@gmail.com>
2020-02-05 19:33:55 -0600

    Added App Component tests in App.test.js and added decrement button to App.js

--- src/App.js
+++ src/App.js
@@ -9,19 +9,23 @@ class App extends Component {
       count: 0
     }
   };
-  makeIncrementer = (amount) => {
+
+  makeIncrementer = amount => () => 
     this.setState(prevState => ({
       count: prevState.count + amount
     }));
-  };
+  
 
   increment = this.makeIncrementer(1);
+
+  decrement = this.makeIncrementer(-1);
 
   render() {
     return (
       <div className="App">
         <h1>Count: {this.state.count}</h1>
         <button className="increment" onClick={this.increment}>Increment Count</button>
+        <button className="decrement" onClick={this.decrement}>Decrement Count</button>
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
 
 test('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<App/>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
-
 
 describe('Addition', () => {
   it('knows that 2 and 2 make 4', () => {
     expect(2 + 2).toBe(4);
   })
 });
+
+describe('App component', () => {
+  it('starts with a count of 0', () => {
+    const wrapper = shallow(<App />);
+    const text = wrapper.find('h1').text();
+    expect(text).toEqual('Count: 0');
+  });
+
+  it('increments count by 1 when increment button is clicked', () => {
+    const wrapper = shallow(<App />);
+    const incrementBtn = wrapper.find('button.increment');
+    incrementBtn.simulate('click');
+    const text = wrapper.find('h1').text();
+    expect(text).toEqual('Count: 1');
+  });
+
+  it('decrements count by 1 when decrement button is clicked', () => {
+    const wrapper = shallow(<App />);
+    const decrementBtn = wrapper.find('button.decrement');
+    decrementBtn.simulate('click');
+    const text = wrapper.find('h1').text();
+    expect(text).toEqual('Count: -1');
+  })
+});
+
