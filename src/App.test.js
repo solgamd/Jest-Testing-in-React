e9097127ccd86eefb41644b3a8e3c0f7b672412c
solgamd <solgamd@gmail.com>
2020-02-05 19:04:58 -0600

    Resolved babel issue by removing node_modules and, rerunning npm i

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders without crashing', () => {
   const div = document.createElement('div');
-  render(<App />, div);
+  ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+
+describe('Addition', () => {
+  it('knows that 2 and 2 make 4', () => {
+    expect(2 + 2).toBe(4);
+  })
 });
